8163b4095dda8424408de2fac33056598cf4b017
Hrishabh412 <hrishabhsoni412@gmail.com>
2018-05-16 05:48:57 +0530

    firstcode

--- a.py
+++ a.py
@@ -3,23 +3,30 @@
 import time
 import webbrowser
 
-print"button1"
-print"button2"
-print"button3"
-print"button4"
-print"button5"
-print"button6"
-print"button7"
+print"press 1 for for string search"
+print"press 2 for for image search"
+print"press 3 for URL name"
+print"press 4 for time and date"
+print"press 5 for default browser"
+print"press 6 for all network ip"
+print"press 7 to get domain and owner name"
 
 x=raw_input()
 
 if x=='1':
-   a=raw_input("enter a string")
-   b=a.split()
-   for i in b:
-      webbrowser.open_new_tab("http://www.google.com/search?btng=1&q=%s" %i)
+   search_data=raw_input("enter a string")
+   new_data=search_data.strip()
+   new_data.split()
+   for i in new_data:
+      webbrowser.open_new_tab("http://www.google.com/search?q="+i)
 elif x=="2":
-   print("image search")
+
+   search_data=raw_input("enter a string")
+   new_data=search_data.strip()
+   new_data.split()
+   for i in new_data:
+      webbrowser.open_new_tab('https://www.google.com/search?q=%s& client=ubuntu&hs=dBh&channel=fs&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiO38XTp4jbAhVEe7wKHUqXB7cQ_AUoA3oECAAQBQ&biw=1535&bih=802='+i)
+     
 elif x=='3':
    print time.ctime()
 
